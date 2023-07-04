26f1bb111e2cbb6fc2e4291d5901a77d61e1163a
DESKTOP-9ORVBHL\Monayem Islam Tamal <monayem.dev@gmail.com>
2023-07-04 17:47:22 +0600

    Link budget calculation logical all operation done

--- src/components/NybsysCalculator.vue
+++ src/components/NybsysCalculator.vue
@@ -14,15 +14,19 @@
               <div class="d-flex justify-content-between">
                 <div class="col">
                   <label for="input1" class="form-label"
-                    >Coverage area (sq. ft) *</label
+                    >Client Required Coverage area (sq. Km) *</label
                   >
                 </div>
+                <!-- <div class="text-primary">
+                  DL: {{ theRequiredNumberOf5GBaseStation }}
+                </div> -->
+
                 <div class="col-4">
                   <input
                     type="number"
                     class="form-control form-control-sm"
                     id="input1"
-                    v-model="totalCoverageArea"
+                    v-model="userDefinedCoverageArea"
                   />
                 </div>
               </div>
@@ -73,11 +77,30 @@
                   <input
                     type="number"
                     class="form-control form-control-sm"
-                    id="input1"
+                    v-model="transmitPower"
                   />
                 </div>
               </div>
             </div>
+            <!-- <div class="row pt-3">
+              <div class="d-flex justify-content-between">
+                <div class="col">
+                  <label class="form-label">Transmit power (dBm) *</label>
+                </div>
+                <div class="col-4">
+                  <select
+                    class="form-select form-select-sm"
+                    aria-label=".form-select-sm example"
+                    v-model="transmitPower"
+                  >
+                    <option value="" selected>Choose an option</option>
+                    <option value="43">43</option>
+                    <option value="44">44</option>
+                    <option value="45">45</option>
+                  </select>
+                </div>
+              </div>
+            </div> -->
             <div class="row pt-3">
               <div class="d-flex justify-content-between">
                 <div class="col">
@@ -89,7 +112,7 @@
                   <input
                     type="number"
                     class="form-control form-control-sm"
-                    id="input1"
+                    v-model="carrierFrequency"
                   />
                 </div>
               </div>
@@ -149,7 +172,7 @@
                   >
                 </div>
                 <div class="col-6 text-end">
-                  <span><b>$50,400.00</b></span>
+                  <span><b>$00000</b></span>
                 </div>
               </div>
             </div>
@@ -163,7 +186,7 @@
                 </div>
                 <div class="col-6 text-end">
                   <span
-                    ><b>{{ totalCoverageArea }}</b></span
+                    ><b>{{ coverageArea }} sq. Km</b></span
                   >
                 </div>
               </div>
@@ -239,11 +262,48 @@ export default {
   components: { BusinessCaseChart },
   data() {
     return {
-      totalCoverageArea: 1000,
+      userDefinedCoverageArea: 7,
       typeOfSite: "",
       specificHardwareCoverageAreaPerCell: null,
       radioPerCell: null,
       concurrentUser: null,
+      numberOfConcurrentNBIoTDevices: null,
+      tcoPeriod: null,
+      numberOfSite: null,
+      numberOfCore: null,
+      indoorArea: null,
+      outdoorArea: null,
+      country: null,
+      frequencyRange: null,
+      privateWirelessTechnology: null,
+      estimatedPriceOfTCO: null,
+      numberOfRANRequired: null,
+      ranUnitPrice: null,
+      proposedCoverageArea: null,
+      requiredNumberOfNBIoTDevice: null,
+      SiteAcquisitionAnnualCost: null,
+      equipmentCost: null,
+      backhaulConnectivityAnnualCost: null,
+      powerSupplyAnnualCost: null,
+      //Link Budget Calculator
+      transmitPower: null,
+      bandwidth: null,
+      antennaGain: null,
+      carrierFrequency: null,
+      baseStationAntennaHeight: null,
+      userTerminalHeight: null,
+      shannonCapacityScalingFactorAlpha: null,
+      cableLoss: null,
+      noiseFigureUrban: null,
+      interferenceMargin: null,
+      cellEdgeReliablity: null,
+      wallPenetarationLoss: null,
+      bodyLoss: null,
+      GUT: null,
+      RxAntennaGain: null,
+      modulationScheme: null,
+      subCarrierSpacing: null,
+      slotsPerSubframe: null,
     };
   },
   watch: {
@@ -251,21 +311,175 @@ export default {
       if (this.typeOfSite == "factory") {
         (this.specificHardwareCoverageAreaPerCell = 200),
           (this.radioPerCell = 3),
-          (this.concurrentUser = 100);
+          (this.concurrentUser = 100),
+          (this.outdoorArea = this.userDefinedCoverageArea),
+          (this.indoorArea = 0),
+          (this.numberOfConcurrentNBIoTDevices = 1000),
+          (this.tcoPeriod = 36),
+          (this.numberOfSite = 1),
+          (this.numberOfCore = 1),
+          (this.estimatedPriceOfTCO = 1000),
+          (this.numberOfRANRequired = 20),
+          (this.ranUnitPrice = 10000),
+          (this.proposedCoverageArea =
+            this.specificHardwareCoverageAreaPerCell),
+          (this.country = "USA"),
+          (this.requiredNumberOfNBIoTDevice =
+            this.numberOfConcurrentNBIoTDevices),
+          (this.SiteAcquisitionAnnualCost = 1000),
+          (this.equipmentCost = 10000),
+          (this.powerSupplyAnnualCost = 500),
+          (this.interferenceMargin = 5),
+          //Link Budget Calculator
+          (this.bandwidth = 10),
+          (this.antennaGain = 18),
+          (this.baseStationAntennaHeight = 20);
+        (this.userTerminalHeight = 1),
+          (this.shannonCapacityScalingFactorAlpha = 0.65),
+          (this.cableLoss = 2),
+          (this.noiseFigureUrban = 5),
+          (this.cellEdgeReliablity = 0.15),
+          (this.wallPenetarationLoss = 18),
+          (this.bodyLoss = 3),
+          (this.GUT = 0),
+          (this.RxAntennaGain = 0);
+        (this.modulationScheme = 256),
+          (this.subCarrierSpacing = 60),
+          (this.slotsPerSubframe = 2);
       } else if (this.typeOfSite == 2) {
-        this.numberOfRadio = 7;
+        return "Nothing";
       }
     },
   },
   computed: {
     numberOfRadio() {
-      return this.cellNumber * this.radioPerCell;
+      return this.theRequiredNumberOf5GBaseStation;
     },
     simCard() {
       return this.numberOfRadio * this.concurrentUser;
     },
     cellNumber() {
-      return this.totalCoverageArea / this.specificHardwareCoverageAreaPerCell;
+      return (
+        this.userDefinedCoverageArea / this.specificHardwareCoverageAreaPerCell
+      );
+    },
+    totalArea() {
+      return this.indoorArea + this.outdoorArea;
+    },
+    requiredNumberOfCPEDevice() {
+      return this.simCard - this.numberOfConcurrentNBIoTDevices;
+    },
+    siteAcquisitionCostTCOPeriod() {
+      return this.SiteAcquisitionAnnualCost * this.tcoPeriod;
+    },
+    backhaulConnectivityCostTCOPeriod() {
+      return this.backhaulConnectivityAnnualCost * this.tcoPeriod;
+    },
+    //link budget calculator
+    equivalentIsotropicallyRadiatedPower() {
+      return this.transmitPower + this.antennaGain - this.cableLoss;
+    },
+    signalToNoiseRatio() {
+      const exponent = 1 / (0.65 * this.bandwidth);
+      const power = Math.pow(2, exponent);
+      const result = 10 * Math.log10(power - 1);
+      return result.toFixed(3);
+    },
+    sensitivityOfTheUserTerminal() {
+      const sensitivityValue =
+        -174 +
+        10 * Math.log10(10 * Math.pow(this.bandwidth, 6)) +
+        this.noiseFigureUrban -
+        this.signalToNoiseRatio;
+      return sensitivityValue.toFixed(3);
+    },
+    shadowingMargin() {
+      // const value =
+      //   Math.sqrt(2) *
+      //   this.shannonCapacityScalingFactorAlpha *
+      //   (2 * this.cellEdgeReliablity);
+      // return value.toFixed(3);
+      return 6.22;
+    },
+    convertCarrierFrequencyInGHz() {
+      return this.carrierFrequency / 1000;
+    },
+    maximumAllowablePathloss() {
+      return (
+        this.equivalentIsotropicallyRadiatedPower +
+        this.GUT -
+        this.sensitivityOfTheUserTerminal -
+        this.interferenceMargin -
+        this.shadowingMargin -
+        this.wallPenetarationLoss -
+        this.bodyLoss
+      );
+    },
+    lineOfSight() {
+      const lineOfSightValue = Math.pow(
+        10,
+        (this.maximumAllowablePathloss +
+          0.6 * (this.userTerminalHeight - 1.5) +
+          13.54 -
+          20 * Math.log10(this.convertCarrierFrequencyInGHz)) /
+          39.08
+      );
+      return lineOfSightValue.toFixed(3);
+    },
+    cellRadiusInMeter() {
+      const squareRootValue = Math.sqrt(
+        Math.pow(this.lineOfSight, 2) -
+          Math.pow(this.baseStationAntennaHeight - this.userTerminalHeight, 2)
+      );
+      return squareRootValue.toFixed(3);
+    },
+    minimalUTSignalLevel() {
+      return "Need data from sajol";
+    },
+    resourceBlocks() {
+      const result =
+        (this.bandwidth * 1000) /
+        (this.subCarrierSpacing * this.slotsPerSubframe);
+      return Math.ceil(result);
+    },
+    resourceBlockBandwidth() {
+      return this.subCarrierSpacing / 1000;
+    },
+    theNoiseBandwidth() {
+      const result = this.resourceBlocks * this.resourceBlockBandwidth;
+      return result.toFixed(3);
+    },
+    numberOfBitsPerSymbol() {
+      const result = Math.log2(this.modulationScheme);
+      return result.toFixed(3);
+    },
+    symbolRate() {
+      const result = this.bandwidth / this.numberOfBitsPerSymbol;
+      return result.toFixed(3);
+    },
+    theReceiverNoiseFloor() {
+      const result =
+        -174 + 10 * Math.log10(this.theNoiseBandwidth) + this.noiseFigureUrban;
+      return result.toFixed(2);
+    },
+    fiveGNRDL() {
+      const result =
+        Math.pow(10, -3) *
+        4 *
+        this.resourceBlocks *
+        this.numberOfBitsPerSymbol *
+        (948 / 1024) *
+        12;
+      return result.toFixed(3);
+    },
+    coverageArea() {
+      const pi = Math.PI;
+      const result = (pi * Math.pow(this.cellRadiusInMeter, 2)) / 1000000;
+      return result.toFixed(3);
+    },
+    theRequiredNumberOf5GBaseStation() {
+      const result = this.userDefinedCoverageArea / this.coverageArea;
+      return Math.ceil(result);
     },
   },
 };
